5675e64120100179a0313f862620ece056e5b966
darus07 <darus07@users.noreply.github.com>
2026-05-17 18:36:14 +0800

    Fix Kriteria 3: Final execution alignment with env-manager none

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -7,11 +7,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-mlflow.autolog()
-
 def main():
     csv_filename = "namadataset_preprocessing.csv"
-    
     if os.path.exists(csv_filename):
         df = pd.read_csv(csv_filename)
     elif os.path.exists(os.path.join("MLProject", csv_filename)):
@@ -33,6 +30,11 @@ def main():
     
     predictions = model.predict(X_test)
     acc = accuracy_score(y_test, predictions)
+    
+    mlflow.log_param("random_state", 42)
+    mlflow.log_metric("accuracy", acc)
+    mlflow.sklearn.log_model(model, "model")
+    
     print(f"Model Accuracy: {acc:.4f}")
 
 if __name__ == "__main__":

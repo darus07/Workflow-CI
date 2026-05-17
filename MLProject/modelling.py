c1eb1cacbb52388c741bf5558f8d27ec1854376a
darus07 <darus07@users.noreply.github.com>
2026-05-17 18:45:39 +0800

    Fix Kriteria 3: Resolve autolog nesting run conflict by enabling nested true

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -27,12 +27,13 @@ def main():
     
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
-    model = RandomForestClassifier(random_state=42)
-    model.fit(X_train, y_train)
-    
-    predictions = model.predict(X_test)
-    acc = accuracy_score(y_test, predictions)
-    print(f"Model Accuracy: {acc:.4f}")
+    with mlflow.start_run(nested=True):
+        model = RandomForestClassifier(random_state=42)
+        model.fit(X_train, y_train)
+        
+        predictions = model.predict(X_test)
+        acc = accuracy_score(y_test, predictions)
+        print(f"Model Accuracy: {acc:.4f}")
 
 if __name__ == "__main__":
     main()
